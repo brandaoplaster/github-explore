f1e1d0ba2af0e0f1fb7a59762cb34d53cda60b3f
brandaoplaster <brandaoplaster@gmail.com>
2020-07-05 20:33:33 -0300

    Creating component list of repositories

--- src/pages/Dasboard/index.tsx
+++ src/pages/Dasboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,21 @@ const Dashboard: React.FC = () => {
         <input placeholder="digite aqui o nome do repository" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="hi">
+          <img
+            src="https://avatars1.githubusercontent.com/u/26599685?s=460&u=08cc3ababd84ad032d357750f19fc356027f5bc8&v=4"
+            alt="Lucas"
+          />
+          <div>
+            <strong>Lucas/github-explore</strong>
+            <p>Platform to create an online portfolio for developers.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
